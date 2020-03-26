b51be77d9964abfc1eecc2399e042459ba8cc801
Jonathan A. Rees <jar398@mumble.net>
2020-03-25 21:42:12 -0400

    improvements to id script

--- clobber_resource_ids.rb
+++ clobber_resource_ids.rb
@@ -1,25 +1,44 @@
-# rails r ad_hoc_id_script.rb
+# bin/rails r clobber_resource_ids.rb
 
 # Run this once, after the resources list has been loaded from the
-# repository (`rake sync`), to adjust the id of each resource
-# so that it matches what's found in file pub-site-resources.json
-# (which is somehow extracted from the publishing server).
+# content repository (`rake sync`), to adjust the id of each resource
+# to match what's in some parallel publishing site, e.g. eol.org.
 
-# Get resources.json file with: wget http://content.eol.org/resources.json
+# For now you must get the resources.json file manually:
+#   wget http://eol.org/resources.json?per_page=1000
+# which creates a local files "resources.json?per_page=1000", whose
+# name is wired into this script.
 
-file = File.read "resources.json"
+file = File.read "resources.json?per_page=1000"
 data = JSON.parse(file)
+seen = {}
 
 count = 0
 wins = 0
-data.each { |resource|
-  id = resource["id"]
-  STDERR.puts("Touching #{resource["name"]}") if (count % 25).zero?
+data["resources"].each { |resource|
+  name = resource["name"]
+  key = name
+  STDERR.puts("Processing: #{key}") if (count % 50).zero?
+  next unless resource["nodes_count"] == nil
   begin 
-    Resource.find_by_name(resource["name"]).update_attribute(:id, resource["id"])
-    wins += 1
-  rescue
-    STDERR.puts("Skipping #{resource["name"]}")
+    record = Resource.find_by(name: name)
+    if record
+      id = resource["id"]
+      if seen.key?(key)
+        r2 = seen[key]
+        a = [r2["id"], r2["name"], r2["nodes_count"]]
+        b = [id, name, resource["nodes_count"]]
+        STDERR.puts("Duplicate key: [#{a}] [#{b}]")
+      else
+        record.update_attribute(:id, id)
+        wins += 1
+        seen[key] = resource
+      end
+    else
+      STDERR.puts("Not in db: #{key}")
+    end
+  rescue => e
+    STDERR.puts("Exception: #{key} #{e}")
   end
   count += 1
 }
